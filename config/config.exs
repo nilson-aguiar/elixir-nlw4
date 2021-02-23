a49006cf69da2abb0ed6c2cc7fd5a17192a5f8de
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2021-02-22 22:55:46 -0300

    Commit inicial - NLW#4 #rumoaoproximonivel

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :rocketpay,
+  ecto_repos: [Rocketpay.Repo]
+
+# Configures the endpoint
+config :rocketpay, RocketpayWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "oKsmWBlcMRwO32fO0Lej4T0j6RrAcfWTMiu8iCQtFURcO+iEaL1Z9PVWQ7AcDA+E",
+  render_errors: [view: RocketpayWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Rocketpay.PubSub,
+  live_view: [signing_salt: "ej29z0Lm"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
